37f96ca00bb2785aa767d70e4256a78496564e3b
LuccaMS <saintangel961@gmail.com>
2021-09-20 15:12:05 -0300

    finished the code and updating the files

--- Dijkstra/MeuGrafo.cpp
+++ Dijkstra/MeuGrafo.cpp
@@ -12,7 +12,7 @@
 #include <vector>
 // std::vector
 #include "excecoes.cpp"
-#include "excecoes.h"
+//#include "excecoes.h"
 // ExcecaoMatrizAdjacenciaVazia
 // ExcecaoMatrizAdjacenciaNaoQuadrada
 // ExcecaoCustoNegativo
@@ -22,13 +22,13 @@
 //implente aqui
 double distanciaMinima(std::vector<double> distancias, std::vector<bool> vetorVisitados)
 {
-    int min = INT_MAX;
+    int min = 5000;
     double index_minimo;
     for (int i = 0; i < distancias.size(); i++)
     {
-        if (vetorVisitados.at(i) == false and distancias.at(i) <= min)
+        if (vetorVisitados[i] == false and distancias[i] <= min)
         {
-            min = distancias.at(i);
+            min = distancias[i];
             index_minimo = i;
         }
     }
@@ -45,13 +45,13 @@ MeuGrafo::MeuGrafo(MatrizAdjacencia const &matrizAdjacencia) : GrafoAbstrato(mat
     {
         for (unsigned int i = 0; i < this->_matrizAdjacencia.size(); i++)
         {
-            for (unsigned int j = 0; j < this->_matrizAdjacencia.at(i).size(); j++)
+            for (unsigned int j = 0; j < this->_matrizAdjacencia[i].size(); j++)
             {
-                if (this->_matrizAdjacencia.at(i).at(j) < 0)
+                if (this->_matrizAdjacencia[i][j] < 0)
                 {
                     throw ExcecaoCustoNegativo();
                 }
-                if (i == j && this->_matrizAdjacencia.at(i).at(j) != inf && this->_matrizAdjacencia.at(i).at(j) > 0)
+                if (i == j && this->_matrizAdjacencia[i][j] != inf && this->_matrizAdjacencia[i][j] > 0)
                 {
                     throw ExcecaoVerticeComLaco();
                 }
@@ -62,19 +62,19 @@ MeuGrafo::MeuGrafo(MatrizAdjacencia const &matrizAdjacencia) : GrafoAbstrato(mat
         int aux = 0;
         for (unsigned int i = 0; i < this->_matrizAdjacencia.size(); i++)
         {
-            vec.push_back(this->_matrizAdjacencia.at(i).size());
+            vec.push_back(this->_matrizAdjacencia[i].size());
         }
 
         if (std::adjacent_find(vec.begin(), vec.end(), std::not_equal_to<>()) == vec.end())
         {
             verify = true;
-            aux = vec.at(0);
+            aux = vec[0];
         }
         bool check_for_wrong_matrix = false;
         int a = 0;
         for (unsigned int i = 1; i < vec.size(); i++)
         {
-            if (vec.at(a) != vec.at(i))
+            if (vec[a] != vec[i])
             {
                 check_for_wrong_matrix = true;
                 break;
@@ -101,7 +101,7 @@ std::size_t MeuGrafo::quantidadeVertices() const
     int count = 0;
     for (unsigned int i = 0; i < this->_matrizAdjacencia.size(); i++)
     {
-        if (this->_matrizAdjacencia.at(i).at(0) == 0)
+        if (this->_matrizAdjacencia[i][0] == 0)
         {
         }
         count++;
@@ -127,28 +127,28 @@ std::vector<double> MeuGrafo::menoresDistancias(std::size_t origem) const
 
         for (int i = 0; i < this->_matrizAdjacencia.size(); i++)
         {
-            vecbool.at(i) = false;
-            distancias.at(i) = inf;
+            vecbool[i] = false;
+            distancias[i] = inf;
         }
 
-        distancias.at(origem) = 0;
+        distancias[origem] = 0;
 
         for (int indice = 0; indice < this->_matrizAdjacencia.size(); indice++)
         {
 
-            double v = distanciaMinima(distancias, vecbool);
+            int v = distanciaMinima(distancias, vecbool);
 
-            vecbool.at(v) = true;
+            vecbool[v] = true;
 
             for (int j = 0; j < this->_matrizAdjacencia.size(); j++)
             {
-                if(!vecbool.at(j))
+                if(!vecbool[j])
                 {
-                    if(distancias.at(v) != inf)
+                    if(distancias[v] != inf)
                     {
-                        if((distancias.at(v) + this->_matrizAdjacencia.at(v).at(j)) < distancias.at(j))
+                        if((distancias[v] + this->_matrizAdjacencia[v][j]) < distancias[j])
                         {
-                            distancias.at(j) = distancias.at(v) + this->_matrizAdjacencia.at(v).at(j);
+                            distancias[j] = distancias[v] + this->_matrizAdjacencia[v][j];
                         }
                     }
                 }
@@ -166,9 +166,9 @@ std::size_t MeuGrafo::quantidadeArestas(std::size_t v) const
     }
 
     int count = 0;
-    for (unsigned int i = 0; i < this->_matrizAdjacencia.at(v).size(); i++)
+    for (unsigned int i = 0; i < this->_matrizAdjacencia[v].size(); i++)
     {
-        if (this->_matrizAdjacencia.at(v).at(i) != inf)
+        if (this->_matrizAdjacencia[v][i] != inf)
         {
             count++;
         }
@@ -182,9 +182,9 @@ std::size_t MeuGrafo::quantidadeArestas() const
     int count = 0;
     for (unsigned int i = 0; i < this->_matrizAdjacencia.size(); i++)
     {
-        for (unsigned int j = 0; j < this->_matrizAdjacencia.at(i).size(); j++)
+        for (unsigned int j = 0; j < this->_matrizAdjacencia[i].size(); j++)
         {
-            if (this->_matrizAdjacencia.at(i).at(j) != inf)
+            if (this->_matrizAdjacencia[i][j] != inf)
             {
                 count++;
             }
@@ -201,7 +201,7 @@ double MeuGrafo::custo(std::size_t origem, std::size_t destino) const
     {
         throw ExcecaoVerticeInvalido();
     }
-    else if (this->_matrizAdjacencia.at(origem).at(destino) < 0)
+    else if (this->_matrizAdjacencia[origem][destino] < 0)
     {
         throw ExcecaoCustoNegativo();
     }
@@ -210,5 +210,5 @@ double MeuGrafo::custo(std::size_t origem, std::size_t destino) const
         return inf;
     }
 
-    return this->_matrizAdjacencia.at(origem).at(destino);
+    return this->_matrizAdjacencia[origem][destino];
 };
